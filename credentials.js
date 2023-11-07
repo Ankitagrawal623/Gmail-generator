b78e58b7b1ec7db7375e4e8bcc1cafa6a0d38a74
Ankit Agarwal <madhusudanjain@Madhusudans-MacBook-Air.local>
2023-11-07 17:09:00 +0530

    gmail generator

---
+++ credentials.js
@@ -0,0 +1,7 @@
+
+const CLIENT_ID = "768267820750-i4culloku44eh70nehar9es5fg4mc3i1.apps.googleusercontent.com";
+const CLEINT_SECRET = "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";
+const REDIRECT_URI = "https://developers.google.com/oauthplayground";
+const REFRESH_TOKEN ="1//04s5Qyl70t_BICgYIARAAGAQSNwF-L9Irh4xp75ewKFDOeaTMDxqdULB5YauQIqRU-JX53VgNt-7crnyKr2XPUA3MmJ0AAY1anpw";
+
+module.exports = { CLIENT_ID, CLEINT_SECRET, REDIRECT_URI, REFRESH_TOKEN };
